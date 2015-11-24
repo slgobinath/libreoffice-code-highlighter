39e29f78f165557cb5b6f45e83f583543325e2a7
Gobinath <slgobinath@gmail.com>
2015-11-24 20:05:18 +0530

    libreoffice-code-highlighter

---
+++ Highlight.py
@@ -0,0 +1,62 @@
+from pygments import styles
+from pygments.lexers import (get_lexer_by_name)
+
+
+def highlight_source_code(*args):
+    ctx = XSCRIPTCONTEXT
+    doc = ctx.getDocument()
+    # Get the selected item
+    selected_item = doc.getCurrentController().getSelection()
+    # Validate that the selected item is a Shape
+    if 'com.sun.star.drawing.Shapes' in selected_item.getSupportedServiceNames():
+        for item_idx in range(selected_item.getCount()):
+            # Get the textbox from the shape
+            box = selected_item.getByIndex(item_idx)
+            if 'com.sun.star.drawing.Text' in box.SupportedServiceNames:
+                # Extract the language name.
+                lang = ''
+                # For Libreoffice Writer, check the Description attribute of the textbox
+                try:
+                    if box.Description.find("code-") == 0:
+                        lang = box.Description.replace("code-", "")
+                except Exception as err:
+                    lang = ''
+
+                # For Libreoffice Impress, check the Style attribute of the textbox
+                if not lang:
+                    try:
+                        if box.Style.getName().find("code-") == 0:
+                            lang = box.Style.getName().replace("code-", "")
+                    except Exception as err:
+                        lang = ''
+
+                if lang:
+                    highlight_code(lang, box)
+
+
+def rgb(r, g, b):
+    return (r & 255) << 16 | (g & 255) << 8 | (b & 255)
+
+
+def to_rgbint(hex_str):
+    if hex_str:
+        r = int(hex_str[:2], 16)
+        g = int(hex_str[2:4], 16)
+        b = int(hex_str[4:], 16)
+        return rgb(r, g, b)
+    return rgb(0, 0, 0)
+
+
+def highlight_code(lang, codebox):
+    code = codebox.String
+    lexer = get_lexer_by_name(lang)
+    cursor = codebox.createTextCursor()
+    style = styles.get_style_by_name('default')
+    cursor.gotoStart(False)
+    for tok_type, tok_value in lexer.get_tokens(code):
+        cursor.goRight(len(tok_value), True)  # selects the token's text
+        cursor.CharColor = to_rgbint(style.style_for_token(tok_type)['color'])
+        cursor.goRight(0, False)  # deselects the selected text
+
+
+g_exportedScripts = (highlight_source_code,)
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # libreoffice-code-highlighter
 Code snippet highlighter for LibreOffice Writer and Impress.
+
+## Installation
+Install libreoffice-script-provider-python
+```
+sudo apt-get install libreoffice-script-provider-python
+```
+
+Install pygments for Python 3:
+```
+sudo pip3 install python3-pygments
+```
+
+Copy the Highlight.py to /usr/lib/libreoffice/share/Scripts/python/
+```
+sudo cp Highlight.py /usr/lib/libreoffice/share/Scripts/python/
+```
+
+Set the Read-Only permission to the script.
+```
+sudo chmod 0444 /usr/lib/libreoffice/share/Scripts/python/Highlight.py
+```
+
+Open the LibreOffice Writer and goto Tools -> Customize -> Keyboard
+- Select the LibreOffice option button (Available on top left corner)
+- Select any desired shortcut
+- Select *share/Highlight/Highlight_source_code* under the Functions Category
+- Click on the Modify button to set the short-cut
+
+
+## Usage
+### LibreOffice Impress
+- Open LibreOffice Impress.
+- Insert a Text Box (Insert -> Text Box).
+- Copy and paste any code snippet into that Text Box.
+- Create a new Style (Right click on an existing style -> New...)
+- Name the Style as code-\<language\>
+- Apply the style to your code snippet Text Box.
+- Select the Text Box.
+- Use your short-cut keys to execute the macro.
+
+### LibreOffice Writer/Calc
+- Open LibreOffice Writer/Calc.
+- Insert a Text Box (Insert -> Text Box).
+- Copy and paste any code snippet into that Text Box.
+- Right click on the Text Box and select 'Description'.
+- Add a description in the following format: code-<language>
+- Select the Text Box
+- Use your short-cut keys to execute the macro.
+
+
+## Credits
+This macro is developed using the following two sources:
+- [impress-code-highlighter](https://github.com/stummjr/impress-code-highlighter)
+- [Python Syntax Highlighting in OpenOffice Impress](http://code.activestate.com/recipes/576796-python-syntax-highlighting-in-openoffice-impress)
